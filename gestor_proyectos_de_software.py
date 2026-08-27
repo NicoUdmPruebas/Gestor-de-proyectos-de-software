e95a7e5b93cde9193a7ac2cd6ea2ac852365040c
Nicolás A <nicolasagudelolopez@gmail.com>
2026-08-27 16:23:08 -0500

    refactor: corrigiendo funciones

--- gestor_proyectos_de_software.py
+++ gestor_proyectos_de_software.py
@@ -1,11 +1,9 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class Colaborador:
     nombre: str
     email: str
-
 
 class Proyecto:
     def __init__(self, nombre: str, lenguaje: str) -> None:
@@ -13,39 +11,41 @@ class Proyecto:
         self.lenguaje = lenguaje
         self.colaboradores: list[Colaborador] = []
 
-    def agregar_colaborador(self, colaborador:Colaborador) -> None | str:
+    def agregar_colaborador(self, colaborador: Colaborador) -> None | str:
         if colaborador not in self.colaboradores:
             self.colaboradores.append(colaborador)
-        return f'Ya existe el colaborador {colaborador}' 
-    
-    def listar_colaboradore(self) -> list:
+        else:
+            return f'Ya existe el colaborador {colaborador.nombre}' 
+
+    def listar_colaboradores(self) -> list[Colaborador]:
         return self.colaboradores
 
-    def tiene_colaborador(self, nombre_colaborador:str ) -> bool:
+    def tiene_colaborador(self, nombre_colaborador: str) -> bool:
         for colaborador_lista in self.colaboradores:
             if nombre_colaborador == colaborador_lista.nombre:
                 return True
         return False
-    def __repr__(self) -> str:
+        
+    def __str__(self) -> str:
         return f'Proyecto: {self.nombre} [{self.lenguaje}] - {len(self.colaboradores)} colaborador(es)'
-    
+
 class GestorProyectos:
     def __init__(self) -> None:
-        self.proyectos: list[Proyecto]=[]
+        self.proyectos: list[Proyecto] = []
 
-    def registrar_proyecto(self, proyecto:Proyecto) -> None | str:
+    def registrar_proyecto(self, proyecto: Proyecto) -> None | str:
         if proyecto not in self.proyectos:
             self.proyectos.append(proyecto)
         else: 
-            return f'Ya existe el proyecto {proyecto}' 
+            return f'Ya existe el proyecto {proyecto.nombre}' 
 
-    def buscar_proyecto(self, nombre_proyecto:str) -> str:
+    def buscar_proyecto(self, nombre_proyecto: str) -> Proyecto | None:
         for proyecto in self.proyectos:
-            if nombre_proyecto == proyecto:
+            if nombre_proyecto == proyecto.nombre: # Corrección: comparar con el atributo nombre
                 return proyecto
-            
+        return None
 
-    def listar_proyectos(self) -> int:
+    def listar_proyectos(self) -> list[Proyecto]:
         return self.proyectos
     
 
